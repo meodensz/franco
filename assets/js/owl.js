9adcbe98ff89e5c658c1966072582013889258cf
kiu <tvt.kiu98@gmail.com>
2020-12-14 16:25:08 +0700

    first commit

---
+++ assets/js/owl.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        767:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+});
